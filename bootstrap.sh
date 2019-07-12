db33dfa5f72bf73632bf06a5110fafbb5f861b8b
Tang <eytang8@gmail.com>
2019-07-11 22:05:26 -0700

    added docker-compose provisioning script

--- bootstrap.sh
+++ bootstrap.sh
@@ -17,3 +17,7 @@ sudo mkdir /data/db
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 sudo usermod -aG docker vagrant
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
